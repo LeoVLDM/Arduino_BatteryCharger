fdb762d9f5a59ca8667918aee2d8f6b31c4e6559
LeoVLDM <leovldm5@gmail.com>
2017-04-17 10:14:44 +0300

    [+] считывание напряжения

--- BatteryCharger.ino
+++ BatteryCharger.ino
@@ -1,13 +1,37 @@
+
+// 1. Считывание напряжения
+/*
+4.88
+170 = 4.14
+*/
+
+// Voltage Sensor 1
+#define vs1_pin A0
+float vs1_in = 0.0;
+float vs1_out = 0.0;
+float vs1_R1 = 30000.0;
+float vs1_R2 = 7500.0;
+
+int value;
+
+
 void setup()
 {
+	pinMode(vs1_pin, INPUT);
 
-  /* add setup code here */
+	Serial.begin(9600);
+	Serial.println("Start Charger");
 
 }
 
 void loop()
 {
+	value = analogRead(vs1_pin);
+	vs1_out= (value * 5.0) / 1024.0;
+	vs1_in = vs1_out / (vs1_R2 / (vs1_R1 + vs1_R2));
 
-  /* add main program code here */
+	Serial.print("INPUT V= ");
+	Serial.println(vs1_in,2);
 
+	delay(250);
 }
